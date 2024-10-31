3988a1834b71537bd81af4d09d96665d889ac7f3
Edwinaeded <edwinaeded@gmail.com>
2024-10-31 15:21:44 +0800

    Build: use express-handlebars as view engine and add bootstrap files for style

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express")
+const { engine } = require("express-handlebars")
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
 app.get('/', (req, res)=>{
-  res.send("hello!world!")
+  res.redirect('/password-generator')
+})
+
+app.get('/password-generator', (req, res) =>{
+  res.render("index")
 })
 
 app.listen(port, ()=>{
   console.log(`express server is running on http://localhost:${port}`)
 })
-
